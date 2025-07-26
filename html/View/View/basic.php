035e3731ff1b5693cdd6e4988dba535eeb0e506d
Anindita-Bhattacharjee-12 <157709960+Anindita-Bhattacharjee-12@users.noreply.github.com>
2025-07-26 11:49:23 +0600

    Initial commit

---
+++ html/View/View/basic.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1> This is web tech section F</h1>
+    <h2> We are seniors right now  </h2>
+    <h2 style="background-color: aqua;">Lets celebrate this</h2>
+    <a href="https://www.aiub.edu/">This is website of aiub</a>
+    <p style="color: blanchedalmond;">This is a stupid things</p>
+    <p style="color: rgb(red, green, blue);">This is intel</p>
+    <img src="images.png" width="100" height="100">
+</body>    
+</html>
